bb8f95992b4c456c2beaab4eaef3feb561c1c7a0
ArzanishS <arzanishking@gmail.com>
2020-11-26 22:56:57 +0000

    added print line with testing

--- HelloPython.py
+++ HelloPython.py
@@ -1,9 +1,8 @@
 import datetime
 print("Hello world!")
-print("Please jenkins work")
 now = datetime.datetime.now()
 print ("Current date and time is ")
 print (now.strftime("%A, %d-%m-%Y : %H:%M"))	
 
-
+print("testing")
 	
