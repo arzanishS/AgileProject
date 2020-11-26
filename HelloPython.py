25183471b1ca83604b1ed39d1f429e408d9b13fe
ArzanishS <arzanishking@gmail.com>
2020-11-26 21:12:44 +0000

    this is 1st commit

--- HelloPython.py
+++ HelloPython.py
@@ -1,9 +1,8 @@
 import datetime
 print("Hello world!")
-print("This is for commit")
 now = datetime.datetime.now()
 print ("Current date and time is ")
 print (now.strftime("%A, %d-%m-%Y : %H:%M"))	
 
-print("testing")
+
 	
